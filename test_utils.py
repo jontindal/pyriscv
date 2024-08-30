ff25bd5d643ad498ffa858440c1a42508e1fe4de
Jon Tindal <jon.tindal@outlook.com>
2024-08-30 16:27:05 +0100

    Add utils file and tests

---
+++ test_utils.py
@@ -0,0 +1,67 @@
+import pytest
+
+import numpy as np
+
+import utils as u
+
+
+@pytest.mark.parametrize(
+    "val,expected",
+    [
+        (0x0000_0001, np.int32(0x0000_0001)),
+        (0x7FFF_FFFF, np.int32(0x7FFF_FFFF)),
+        (-0x0000_0001, np.int32(-0x0000_0001)),
+        (-0x8000_0000, np.int32(-0x8000_0000)),
+        (0x8000_0000, np.int32(-0x8000_0000)),
+        (0xFFFF_FFFF, np.int32(-0x0000_0001)),
+        (np.uint32(0x0000_0001), np.int32(0x0000_0001)),
+        (np.uint32(0x7FFF_FFFF), np.int32(0x7FFF_FFFF)),
+        (np.uint32(0x8000_0000), np.int32(-0x8000_0000)),
+        (np.uint32(0xFFFF_FFFF), np.int32(-0x0000_0001)),
+    ],
+)
+def test_to_int32(val: u.IntTypes, expected: np.int32):
+    result = u.to_int32(val)
+    assert (
+        result == expected
+    ), f"Got 0x{result:x}, expected 0x{expected:x} for input 0x{val:x}"
+
+
+@pytest.mark.parametrize(
+    "val,expected",
+    [
+        (0x0000_0001, np.uint32(0x0000_0001)),
+        (0x7FFF_FFFF, np.uint32(0x7FFF_FFFF)),
+        (-0x0000_0001, np.uint32(0xFFFF_FFFF)),
+        (-0x8000_0000, np.uint32(0x8000_0000)),
+        (0x8000_0000, np.uint32(0x8000_0000)),
+        (0xFFFF_FFFF, np.uint32(0xFFFF_FFFF)),
+        (np.int32(0x0000_0001), np.uint32(0x0000_0001)),
+        (np.int32(0x7FFF_FFFF), np.uint32(0x7FFF_FFFF)),
+        (np.int32(-0x0000_0001), np.uint32(0xFFFF_FFFF)),
+        (np.int32(-0x8000_0000), np.uint32(0x8000_0000)),
+    ],
+)
+def test_to_uint32(val: u.IntTypes, expected: np.uint32):
+    result = u.to_uint32(val)
+    assert (
+        result == expected
+    ), f"Got 0x{result:x}, expected 0x{expected:x} for input 0x{val:x}"
+
+
+@pytest.mark.parametrize("val,width,expected", [(0x1, 3, "001"), (-0x1, 3, "111")])
+def test_int_to_bits(val: u.IntTypes, width: int, expected: str):
+    result = u.int_to_bits(val, width)
+    assert result == expected, f"Got {result}, expected {expected}"
+
+
+@pytest.mark.parametrize("val,expected", [("001", 1), ("111", 7)])
+def test_bits_to_uint(val: str, expected: int):
+    result = u.bits_to_uint(val)
+    assert result == expected, f"Got {result}, expected {expected}"
+
+
+@pytest.mark.parametrize("val,expected", [("001", 1), ("111", -1)])
+def test_bits_to_int(val: str, expected: int):
+    result = u.bits_to_int(val)
+    assert result == expected, f"Got {result}, expected {expected}"
--- utils.py
+++ utils.py
@@ -1,16 +1,42 @@
 import numpy as np
 
 
-def int_to_bits(val: int, width: int) -> str:
+IntTypes = int | np.uint32 | np.int32
+
+
+def to_uint32(val: IntTypes) -> np.uint32:
+    int_val = int(val)
+    if int_val < 0:
+        int_val += 1 << 32
+    return np.uint32(int_val)
+
+
+def to_int32(val: IntTypes) -> np.int32:
+    int_val = int(val)
+    if int_val >= (1 << 31):
+        int_val -= 1 << 32
+    return np.int32(int_val)
+
+
+def int_to_bits(val: IntTypes, width: int) -> str:
+    """Get binary representation as a string.
+    Uses two's complement for negative numbers"""
     return np.binary_repr(val, width)
 
 
-def bits_to_int(bits: str) -> int:
+def bits_to_uint(bits: str) -> int:
+    """Convert binary string to unsigned int"""
     return int(bits, 2)
 
 
-def bitfield_slice(bits: str, high: int, low: int):
-    """extract bitfields from a bit-array using Verilog bit-indexing order,
+def bits_to_int(bits: str) -> int:
+    """Convert binary string to signed int using two's complement"""
+    sign_bit = int(bits[0])
+    return int(bits[1:], 2) - (sign_bit << (len(bits) - 1))
+
+
+def bitfield_slice(bits: str, high: int, low: int) -> str:
+    """Extract bitfields from a bit-array using Verilog bit-indexing order,
     so [0] is the right-most bit (which is opposite order than bitstring),
     and [1:0] are the 2 least significant bits, etc."""
     return bits[len(bits) - 1 - high: len(bits) - low]
