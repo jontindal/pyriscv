3a39ddd966a08b0832d364680d9ce24507d1b1e6
Jon Tindal <jon.tindal@outlook.com>
2025-07-13 15:07:39 +0100

    Cleanup riscof_pyriscv.py script

--- riscof/pyriscv/riscof_pyriscv.py
+++ riscof/pyriscv/riscof_pyriscv.py
@@ -1,5 +1,5 @@
 import logging
-import os
+import pathlib
 
 import riscof.utils as utils
 from riscof.pluginTemplate import pluginTemplate
@@ -12,168 +12,98 @@ class pyriscv(pluginTemplate):
 
     __version__ = "0.1.0"
 
-    def __init__(self, *args, **kwargs):
+    def __init__(self, *args, config: dict, **kwargs):
         super().__init__(*args, **kwargs)
 
-        config = kwargs.get("config")
+        self.dut_exe = pathlib.Path(config.get("PATH", "")) / "pyriscv-riscof"
+        self.num_jobs = str(config.get("jobs", 1))  # Number of jobs for make
+        self.pluginpath = pathlib.Path(config["pluginpath"]).resolve()
+        self.isa_spec = pathlib.Path(config["ispec"]).resolve()
+        self.platform_spec = pathlib.Path(config["pspec"]).resolve()
 
-        # If the config node for this DUT is missing or empty. Raise an error. At minimum we need
-        # the paths to the ispec and pspec files
-        if config is None:
-            print("Please enter input file paths in configuration.")
-            raise SystemExit(1)
-
-        # In case of an RTL based DUT, this would be point to the final binary executable of your
-        # test-bench produced by a simulator (like verilator, vcs, incisive, etc). In case of an iss or
-        # emulator, this variable could point to where the iss binary is located. If 'PATH variable
-        # is missing in the config.ini we can hardcode the alternate here.
-        self.dut_exe = os.path.join(
-            config["PATH"] if "PATH" in config else "", "pyriscv-riscof"
-        )
-
-        # Number of parallel jobs that can be spawned off by RISCOF
-        # for various actions performed in later functions, specifically to run the tests in
-        # parallel on the DUT executable. Can also be used in the build function if required.
-        self.num_jobs = str(config["jobs"] if "jobs" in config else 1)
-
-        # Path to the directory where this python file is located. Collect it from the config.ini
-        self.pluginpath = os.path.abspath(config["pluginpath"])
-
-        # Collect the paths to the  riscv-config based ISA and platform yaml files. One can choose
-        # to hardcode these here itself instead of picking it from the config.ini file.
-        self.isa_spec = os.path.abspath(config["ispec"])
-        self.platform_spec = os.path.abspath(config["pspec"])
-
-        # We capture if the user would like the run the tests on the target or
-        # not. If you are interested in just compiling the tests and not running
-        # them on the target, then following variable should be set to False
         if "target_run" in config and config["target_run"] == "0":
+            # To only compile tests, not run on target
             self.target_run = False
         else:
             self.target_run = True
 
-    def initialise(self, suite, work_dir, archtest_env):
-        # capture the working directory. Any artifacts that the DUT creates should be placed in this
-        # directory. Other artifacts from the framework and the Reference plugin will also be placed
-        # here itself.
-        self.work_dir = work_dir
+    def initialise(self, suite: str, workdir: str, env: str):
+        # Working directory to store artifacts from DUT, framework and reference plugin
+        self.work_dir = workdir
 
-        # capture the architectural test-suite directory.
+        # Architectural test-suite directory
         self.suite_dir = suite
 
-        # Note the march is not hardwired here, because it will change for each
-        # test. Similarly the output elf name and compile macros will be assigned later in the
-        # runTests function
-        self.compile_cmd = (
-            "riscv{1}-unknown-elf-gcc -march={0} \
-            -static -mcmodel=medany -fvisibility=hidden -nostdlib -nostartfiles -g\
-            -T "
-            + self.pluginpath
-            + "/env/link.ld\
-            -I "
-            + self.pluginpath
-            + "/env/\
-            -I "
-            + archtest_env
-            + " {2} -o {3} {4}"
-        )
+        linker_path = self.pluginpath / "env" / "link.ld"
+        include_path = self.pluginpath / "env"
 
-    def build(self, isa_yaml, platform_yaml):
-        # load the isa yaml as a dictionary in python.
+        self.compile_args = [
+            "-static",
+            "-mcmodel=medany",
+            "-fvisibility=hidden",
+            "-nostdlib",
+            "-nostartfiles",
+            "-g",
+            f"-T {linker_path}",
+            f"-I {include_path}",
+            f"-I {env}",
+        ]
+
+    def build(self, isa_yaml: str, platform_yaml: str):
         ispec = utils.load_yaml(isa_yaml)["hart0"]
 
-        # capture the XLEN value by picking the max value in 'supported_xlen' field of isa yaml. This
-        # will be useful in setting integer value in the compiler string (if not already hardcoded);
+        # Capture the XLEN value by picking the max value in 'supported_xlen' field of isa yaml
         self.xlen = "64" if 64 in ispec["supported_xlen"] else "32"
 
-        # for pyriscv start building the '--isa' argument. the self.isa is dutnmae specific and may not be
-        # useful for all DUTs
-        self.isa = "rv" + self.xlen
-        if "I" in ispec["ISA"]:
-            self.isa += "i"
-        if "M" in ispec["ISA"]:
-            self.isa += "m"
-        if "F" in ispec["ISA"]:
-            self.isa += "f"
-        if "D" in ispec["ISA"]:
-            self.isa += "d"
-        if "C" in ispec["ISA"]:
-            self.isa += "c"
+        self.compile_cmd = f"riscv{self.xlen}-unknown-elf-gcc"
 
-        self.compile_cmd = (
-            self.compile_cmd
-            + " -mabi="
-            + ("lp64 " if 64 in ispec["supported_xlen"] else "ilp32 ")
-        )
+        abi = "lp64" if 64 in ispec["supported_xlen"] else "ilp32"
+        self.compile_args.append(f"-mabi={abi}")
 
-    def runTests(self, testList):
-        # Delete Makefile if it already exists.
-        if os.path.exists(self.work_dir + "/Makefile." + self.name[:-1]):
-            os.remove(self.work_dir + "/Makefile." + self.name[:-1])
-        # create an instance the makeUtil class that we will use to create targets.
+    def runTests(self, testlist: dict):
+        makefile_path = pathlib.Path(self.work_dir) / f"Makefile.{self.name[:-1]}"
+        if makefile_path.exists():
+            makefile_path.unlink()
+
         make = utils.makeUtil(
-            makefilePath=os.path.join(self.work_dir, "Makefile." + self.name[:-1])
+            makeCommand=f"make -k -j{self.num_jobs}",
+            makefilePath=str(makefile_path),
         )
 
-        # set the make command that will be used. The num_jobs parameter was set in the __init__
-        # function earlier
-        make.makeCommand = "make -k -j" + self.num_jobs
-
-        # we will iterate over each entry in the testList. Each entry node will be referred to by the
-        # variable testname.
-        for testname in testList:
-            # for each testname we get all its fields (as described by the testList format)
-            testentry = testList[testname]
-
-            # we capture the path to the assembly file of this test
+        for testname, testentry in testlist.items():
             test = testentry["test_path"]
 
-            # capture the directory where the artifacts of this test will be dumped/created. RISCOF is
-            # going to look into this directory for the signature files
-            test_dir = testentry["work_dir"]
+            test_dir = pathlib.Path(testentry["work_dir"])
 
-            # name of the elf file after compilation of the test
             elf_file = "test.elf"
             bin_file = "test.bin"
 
-            # name of the signature file as per requirement of RISCOF. RISCOF expects the signature to
-            # be named as DUT-<dut-name>.signature. The below variable creates an absolute path of
-            # signature file.
-            sig_file = os.path.join(test_dir, self.name[:-1] + ".signature")
+            # RISCOF expects the signature to be named as DUT-<dut-name>.signature
+            sig_file = test_dir / f"{self.name[:-1]}.signature"
 
-            # for each test there are specific compile macros that need to be enabled. The macros in
-            # the testList node only contain the macros/values. For the gcc toolchain we need to
-            # prefix with "-D". The following does precisely that.
-            compile_macros = " -D" + " -D".join(testentry["macros"])
-
-            # substitute all variables in the compile command that we created in the initialize
-            # function
-            compile_cmd = self.compile_cmd.format(
-                testentry["isa"].lower(), self.xlen, test, elf_file, compile_macros
+            compile_args = self.compile_args.copy()
+            compile_args.extend(
+                [
+                    *[f"-D{macro}" for macro in testentry["macros"]],
+                    f"-march={testentry['isa'].lower()}",
+                    f"-o {elf_file}",
+                    test,
+                ]
             )
 
-            copy_command = f"riscv{self.xlen}-unknown-elf-objcopy --strip-all -O binary {elf_file} {bin_file}"
+            compile_cmd = " ".join([self.compile_cmd] + compile_args)
+            copy_cmd = f"riscv{self.xlen}-unknown-elf-objcopy --strip-all -O binary {elf_file} {bin_file}"
 
-            # if the user wants to disable running the tests and only compile the tests, then
-            # the "else" clause is executed below assigning the sim command to simple no action
-            # echo statement.
             if self.target_run:
-                # set up the simulation command. Template is for spike. Please change.
                 simcmd = f"{self.dut_exe} {bin_file} --test-signature={sig_file}"
             else:
                 simcmd = 'echo "NO RUN"'
 
-            # concatenate all commands that need to be executed within a make-target.
             execute = (
-                f"@cd {testentry['work_dir']}; {compile_cmd}; {copy_command}; {simcmd};"
+                f"@cd {testentry['work_dir']}; {compile_cmd}; {copy_cmd}; {simcmd};"
             )
-
-            # create a target. The makeutil will create a target with the name "TARGET<num>" where num
-            # starts from 0 and increments automatically for each new target that is added
             make.add_target(execute)
 
-        # once the make-targets are done and the makefile has been created, run all the targets in
-        # parallel using the make command set above.
         make.execute_all(self.work_dir)
 
         # if target runs are not required then we simply exit as this point after running all
